7b6381316a7b2b524aac47d9d5ce71f086af2b84
Didac Gomez <didac.programacio@gmail.com>
2022-03-24 19:23:38 +0100

    funcionalitats de l'aplicació

--- users.py
+++ users.py
@@ -1,11 +1,14 @@
 import tkinter as tk
 from tkinter import messagebox
-from constatnts import ACTIVE_OPTION, BG_BUTTON, OPTION_FONT
+from constatnts import ACTIVE_OPTION, BG_BUTTON, DATA_BASE, OPTION_FONT, TABLE
+from data_base import run_sql
 
 
 class Users(tk.Frame):
     def __init__(self, window):
         super(Users, self).__init__(window)
+        # Variables
+        self.name_db = DATA_BASE
         # Frame options
         self.frame_options = tk.Frame(self)
         self.opt_login = tk.Label(self.frame_options, text=" Login ", cursor="hand2", font=OPTION_FONT, bg="red")
@@ -37,6 +40,7 @@ class Users(tk.Frame):
             command=self.signup)
         self.draw()
         self.draw_login()
+        self.create_db()
 
     def draw(self):
         self.pack(fill=tk.BOTH)
@@ -69,3 +73,85 @@ class Users(tk.Frame):
         self.txt_signup_confirm_pass.pack(padx=10)
         self.bt_signup.pack(padx=20, pady=(15, 0))
         self.txt_signup_user.focus()
+        
+    def change_option(self, event):
+        if event.widget == self.opt_login:
+            self.draw_login()
+        elif event.widget == self.opt_signup:
+            self.draw_signup()
+
+    def check_login_fields(self):
+        if self.txt_login_user.get() != "" and self.txt_login_pass.get() != "":
+            return True
+        return messagebox.showerror("Login", "Must fill the fields")
+
+    def check_signup_fields(self):
+        if (self.txt_signup_user.get() != "" and self.txt_signup_pass.get() != "" and 
+            self.txt_signup_confirm_pass.get() != ""):
+            return True
+        return messagebox.showerror("Sign up", "Must fill the fields")
+
+    def check_passwords(self):
+        if self.txt_signup_pass.get() == self.txt_signup_confirm_pass.get():
+            return True
+        return messagebox.showerror("Sign up", "The passwords have to be equals")
+
+    def delete_fields(self, login):
+        if login:
+            self.txt_login_user.delete(0, tk.END)
+            self.txt_login_pass.delete(0, tk.END)
+            self.txt_login_user.focus()
+        else:
+            self.txt_signup_user.delete(0, tk.END)
+            self.txt_signup_pass.delete(0, tk.END)
+            self.txt_signup_confirm_pass.delete(0, tk.END)
+            self.txt_signup_user.focus()
+
+    def login(self):
+        if self.check_login_fields() == True:
+            if self.check_user(self.txt_login_user.get(), True) == self.txt_login_pass.get():
+                messagebox.showinfo("Login", "You have accessed")
+
+    def signup(self):
+        if self.check_signup_fields() == True:
+            if self.check_passwords() == True:
+                if self.check_user(self.txt_signup_user.get(), False):
+                    self.add_user()
+                    self.delete_fields(False)
+
+    def create_db(self):
+        sql = TABLE
+        run_sql(name=self.name_db, query=sql)
+
+    def fin_id(self):
+        num_id = 1
+        sql = "SELECT ID FROM USERS ORDER BY ID ASC"
+        data = run_sql(name=self.name_db, query=sql, read=True)
+        for item in data:
+            if item[0] != num_id:
+                break
+            num_id += 1
+        return num_id
+
+    def check_user(self, user, login):
+        sql = "SELECT PASSWORD FROM USERS WHERE USER=?"
+        params = (user, )
+        item = run_sql(name=self.name_db, query=sql, parameters=params, read=True)
+        if login:
+            if item:
+                return item[0][0]
+            messagebox.showerror("Login", "The user doesn't exist.")
+            self.delete_fields(True)
+            return False
+        else:
+            if item:
+                messagebox.showerror("Sign up", "The user just exists.")
+                self.delete_fields(False)
+                return False
+            return True
+
+    def add_user(self):
+        sql = "INSERT INTO USERS VALUES(?, ?, ?)"
+        params = (self.fin_id(), self.txt_login_user.get(), self.txt_login_pass.get())
+        run_sql(name=self.name_db, query=sql, parameters=params)
+        messagebox.showinfo("Sign up", "User creates successfully")
